306784b398672db5812e4e8115a184c522e9308a
Philipp Balzarek <p.balzarek@googlemail.com>
2019-07-10 16:59:04 +0200

    Export Network.WebSockets.{Http,Protocol} via Internal module

---
+++ src/Network/WebSockets/Internal.hs
@@ -0,0 +1,7 @@
+module Network.WebSockets.Internal
+  ( module Network.WebSockets.Http
+  , module Network.WebSockets.Protocol
+  ) where
+
+import Network.WebSockets.Http
+import Network.WebSockets.Protocol
